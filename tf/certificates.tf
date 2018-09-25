747b5ac092d0c8236ed2103675f8c69174f2c3f6
Adam Christie <fractos@gmail.com>
2018-09-25 22:57:58 +0100

    fmt

--- tf/certificates.tf
+++ tf/certificates.tf
@@ -1,10 +1,12 @@
 # Certificates
 
 resource "aws_acm_certificate" "base" {
-  domain_name       = "${var.domain}"
+  domain_name = "${var.domain}"
+
   subject_alternative_names = [
-    "*.${var.domain}"
+    "*.${var.domain}",
   ]
+
   validation_method = "DNS"
 
   tags {
