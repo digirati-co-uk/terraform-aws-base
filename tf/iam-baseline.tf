9853befe345593c125da75a97787875fdda5ddb0
Adam Christie <fractos@gmail.com>
2018-10-11 09:54:58 +0100

    fmt

--- tf/iam-baseline.tf
+++ tf/iam-baseline.tf
@@ -1,6 +1,7 @@
 module "iam_baseline" {
-  source     = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/iam_baseline/"
-  project    = "${var.project}"
-  prefix     = "${var.prefix}"
-  account_id = "${var.account_id}"
+  source        = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/iam_baseline/"
+  project       = "${var.project}"
+  prefix        = "${var.prefix}"
+  account_id    = "${var.account_id}"
+  account_alias = "${var.account_alias}"
 }
