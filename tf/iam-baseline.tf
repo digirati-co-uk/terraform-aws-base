8bd4cc8b62fc4604be2b047074604e1f1eb00290
Adam Christie <fractos@gmail.com>
2018-10-11 10:43:16 +0100

    typo.

--- tf/iam-baseline.tf
+++ tf/iam-baseline.tf
@@ -1,5 +1,5 @@
 module "iam_baseline" {
-  source        = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/iam_baseline/"
+  source        = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/iam-baseline/"
   project       = "${var.project}"
   prefix        = "${var.prefix}"
   account_id    = "${var.account_id}"
